b7e913c1d64aa3a8ebb3d9f06c75f9d072a4c407
ash <aishwarya.chaturvedi050@gmail.com>
2014-03-08 17:08:44 +0000

    added menu system

--- js/ashstrap.js
+++ js/ashstrap.js
@@ -76,56 +76,73 @@ for (i = 0;i<array.length;i++){
 };
 
 /*
-click function below
+click and keypress function below
 */
 document.onkeydown = checkKey;
-  function checkKey(e){
-    e = e || window.event;
-      if (e.keyCode == '39'){
-        movenext();
-      }
-      if (e.keyCode == '37'){
-        moveprev();
-      }
+
+function checkKey(e){
+  e = e || window.event;
+    if (e.keyCode == '39'){
+      movenext();
+    }
+    if (e.keyCode == '37'){
+      moveprev();
+    }
+}
+
+$(".next").click(function(){
+  movenext();
+});
+
+
+$(".prev").click(function(){
+  moveprev();
+});
+
+function movenext(){
+  if(!$(pages).next().length){
+    console.log("no more pages to right");
   }
+  else{
+    $(cur).next().addClass("current").animate({
+        marginLeft:"auto"
+    },{duration:300,queue:false});
 
-  $(".next").click(function(){
-    movenext();
-  });
+    $(".current").prev().removeClass("current").animate({
+      marginLeft:"-" + windowWidth + "px"
+    },{duration:300,queue:false});
 
-  
-  $(".prev").click(function(){
-    moveprev();
-  });
+  }
+};
 
- function movenext(){
-    if(!$(pages).next().length){
-      console.log("no more pages to right");
+function moveprev(){
+  if(!$(pages).prev().length){
+    console.log("no more pages to left");
+  }
+  else{
+
+      $(cur).prev().addClass("current").animate({
+        marginLeft:"0px"
+      },{duration:300,queue: false});
+
+      $(".current").next().removeClass("current").animate({  
+        marginLeft:"auto"
+      },{duration:300,queue:false}); 
+  } 
+};
+
+
+/*
+Menu system
+*/
+$(".link").click(function(){
+    for (z = 0; z<array.length; z++){
+      if ($(this).attr("data-role") == array[z].id){
+        var t = $(this).attr("data-role");
+        $("section#"+t).prevAll().removeClass("current").css("marginLeft","-"+windowWidth+"px");
+        $("section#"+t).nextAll().removeClass("current").css("marginLeft","auto");
+        $("section#"+t).addClass("current").css("marginLeft","0px");
+      }
     }
-    else{
-      $(cur).next().addClass("current").animate({
-          marginLeft:"auto"
-      },{duration:300,queue:false});
+});
 
-      $(".current").prev().removeClass("current").animate({
-        marginLeft:"-" + windowWidth + "px"
-      },{duration:300,queue:false});
-
-    }
-  };
-
-  function moveprev(){
-    if(!$(pages).prev().length){
-      console.log("no more pages to left");
-    }
-    else{
-
-        $(cur).prev().addClass("current").animate({
-          marginLeft:"0px"
-        },{duration:300,queue: false});
-
-        $(".current").next().removeClass("current").animate({  
-          marginLeft:"auto"
-        },{duration:300,queue:false}); 
-    } 
-  };
